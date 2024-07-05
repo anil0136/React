2d76408301286899f4b535ac6448055a8ad236e1
anil0136 <anilmaikap0136@gmail.com>
2024-07-05 23:36:57 +0530

    React

--- src/App.js
+++ src/App.js
@@ -3,22 +3,23 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="gallery">
+        <div className="grid">
+            <img src={require("./image/image1.jpg")} alt="Image 1" className="grid-item" />
+            <img src={require("./image/image2.jpg")} alt="Image 2" className="grid-item" />
+            <img src={require("./image/image3.jpg")} alt="Image 3" className="grid-item" />
+            <img src={require("./image/image4.jpg")} alt="Image 4" className="grid-item" />
+            <img src={require("./image/image5.jpg")} alt="Image 5" className="grid-item" />
+            <img src={require("./image/image6.jpg")} alt="Image 6" className="grid-item" />
+        </div>
+        <div className="lightbox">
+            <div className="lightbox-content">
+                <img src="" alt="" className="lightbox-image" />
+                <span className="lightbox-close">&times;</span>
+            </div>
+        </div>
     </div>
+
   );
 }
 
